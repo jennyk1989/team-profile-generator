4f25d59bf475134f34250d2d3500aae3ee260bdd
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-01-14 14:48:25 -0600

    employee tests added & required modules for index.js

---
+++ __test__/Employee.test.js
@@ -0,0 +1,41 @@
+// unit test for Employee class/constructor
+
+// import Employee module
+const { expect } = require("@jest/globals");
+const Employee = require("../lib/Employee");
+
+// import mock data
+jest.mock('../lib/Employee.js'); //mocks Employee implementation with faked data
+// test name, id, & email Employee properties
+test ('creates an Employee object', () => {
+    const employee = new Employee('name', 'id', 'email');
+    //expect employee's name to be a string
+    expect(employee.name).toEqual(expect.any(String));
+    //expect employee's id to be a number
+    expect(employee.id).toEqual(expect.any(Number));
+    //expect employee's email to be a string
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+// test getName() method
+test('gets name of employee', () => {
+    const employee = new Employee('name');
+    expect(employee.getName()).toEqual(expect.stringContaining('name'));
+})
+// test getID() method
+test('gets ID of employee', () => {
+    const employee = new Employee('name', 'id', 'email');
+    expect(employee.getID()).toEqual(expect.stringContaining('id'));
+})
+
+// test getEmail() method
+test('gets email of employee', () => {
+    const employee = new Employee('name', 'id', 'email');
+    expect(employee.getEmail()).toEqual(expect.stringContaining('email'));
+})
+
+// test getRole() method
+test('gets role of employee', () => {
+    const employee = new Employee('name', 'id', 'email');
+    expect(employee.getRole().toEqual('Employee'));
+})
---
+++ index.js
@@ -0,0 +1,4 @@
+//import required Node modules
+const fs = require('fs'); //FileSystem module for writing files
+const inquirer = require('inquirer'); //Inquirer module for handling user prompts
+
