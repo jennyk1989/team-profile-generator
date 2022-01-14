3030ef5031bf4df3c8cfd93900e3ed7b1d49f51f
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-01-14 16:00:51 -0600

    html mockup

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 const fs = require('fs'); //FileSystem module for writing files
 const inquirer = require('inquirer'); //Inquirer module for handling user prompts
 
+//import other required files
+//Employee.js not required (imported via other files)
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
